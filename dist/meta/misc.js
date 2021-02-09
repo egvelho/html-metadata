1d0a96254653cd6ceb13ee0fb33a8434af35500b
Eduardo Velho <eduardo.velho@icloud.com>
2021-02-09 20:06:38 -0300

    Fix logic error at misc.tsx

--- dist/meta/misc.js
+++ dist/meta/misc.js
@@ -6,12 +6,9 @@ var react_1 = tslib_1.__importDefault(require("react"));
 function MetaMisc(_a) {
     var facebookAppId = _a.facebookAppId, twitterAt = _a.twitterAt, url = _a.url, name = _a.name, dashColor = _a.dashColor;
     return (react_1.default.createElement(react_1.default.Fragment, null,
-        "facebookAppId && ",
-        react_1.default.createElement("meta", { property: "fb:app_id", content: facebookAppId }),
-        "twitterAt && ",
-        react_1.default.createElement("meta", { name: "twitter:creator", content: twitterAt }),
-        "twitterAt && ",
-        react_1.default.createElement("meta", { name: "twitter:site", content: twitterAt }),
+        facebookAppId && react_1.default.createElement("meta", { property: "fb:app_id", content: facebookAppId }),
+        twitterAt && react_1.default.createElement("meta", { name: "twitter:creator", content: twitterAt }),
+        twitterAt && react_1.default.createElement("meta", { name: "twitter:site", content: twitterAt }),
         react_1.default.createElement("meta", { name: "url", content: url }),
         react_1.default.createElement("meta", { property: "og:site_name", content: name }),
         react_1.default.createElement("meta", { name: "application-name", content: name }),
--- src/meta/misc.tsx
+++ src/meta/misc.tsx
@@ -15,9 +15,9 @@ export function MetaMisc({
 }) {
   return (
     <>
-      facebookAppId && <meta property="fb:app_id" content={facebookAppId} />
-      twitterAt && <meta name="twitter:creator" content={twitterAt} />
-      twitterAt && <meta name="twitter:site" content={twitterAt} />
+      {facebookAppId && <meta property="fb:app_id" content={facebookAppId} />}
+      {twitterAt && <meta name="twitter:creator" content={twitterAt} />}
+      {twitterAt && <meta name="twitter:site" content={twitterAt} />}
       <meta name="url" content={url} />
       <meta property="og:site_name" content={name} />
       <meta name="application-name" content={name} />
